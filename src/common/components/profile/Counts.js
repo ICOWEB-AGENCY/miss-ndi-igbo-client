f756f571f54ba40c29759a4b9906c73a91ad4e21
George, B. Smith <georgebsmith.tech@gmail.com>
2021-10-11 00:30:58 +0100

    fixed vote count

--- src/common/components/profile/Counts.js
+++ src/common/components/profile/Counts.js
@@ -5,14 +5,14 @@ const Counts=({user})=>{
         <div style={{display:"flex",margin:"72px 0px 80px 0px",justifyContent:"center"}}>
 <VotesAndShares 
 text="LINK SHARES"
-user={user}
+val={user.shareCount}
 />
 <div style={{width:1,height:88,backgroundColor:"rgba(196, 196, 196, 1)"}}>
 
 </div>
 <VotesAndShares
 text="VOTES"
-user={user}
+val={user.votes}
  />
 
 </div>
--- src/common/components/profile/VotesAndShares.js
+++ src/common/components/profile/VotesAndShares.js
@@ -1,8 +1,8 @@
-const VotesAndShares =({text,user={}})=>{
+const VotesAndShares =({text,val=0})=>{
     return (
         <div style={{margin:"0px 45px",display:"flex",flexDirection:"column",alignItems:"center"}}>
 <p style={{fontSize:32,fontWeight:"600",color:"rgba(58, 33, 16, 1)",marginBottom:16}}>
-{user.shareCount}
+{val}
 </p>
 <p style={{fontSize:12,color:"rgba(159, 135, 114, 1)"}}>
 {text}
