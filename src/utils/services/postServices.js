bb838c78aca08176da6f971beac60d222edcab19
George, B. Smith <georgebsmith.tech@gmail.com>
2021-11-17 21:46:13 +0100

    added nothing

--- src/utils/services/postServices.js
+++ src/utils/services/postServices.js
@@ -1,58 +1,39 @@
-import axios from "axios" 
-import baseURL from "../../configs/baseURL"
-import cookie from 'js-cookie'
+import axios from "axios";
+import baseURL from "../../configs/baseURL";
+import cookie from "js-cookie";
 
-export const logUser=async (data)=>{
+export const logUser = async (data) => {
+  try {
+    const response = await axios.post(`${baseURL}/admin/auth/login`, data);
 
-    try {
-        const response = await axios.post(`${baseURL}/auth/login`,data)
+    return response.data;
+  } catch (error) {
+    console.log(error.response.data);
+    return error.response.data;
+  }
+};
 
-return response.data
-        
-    } catch (error) {
-        console.log(error.response.data)
-        return error.response.data
-        
-    }
+export const postData = async (data, path) => {
+  try {
+    const response = await axios.post(`${baseURL}/` + path, data);
 
-}
-
-
-export const postData=async (data,path)=>{
-
-    try {
-        const response = await axios.post(`${baseURL}/`+path,data)
-
-return response.data
-        
-    } catch (error) {
-        console.log(error.response.data)
-        return error.response.data
-        
-    }
-
-}
+    return response.data;
+  } catch (error) {
+    console.log(error.response.data);
+    return error.response.data;
+  }
+};
 export const postProtectedData = async (data, path, tokens) => {
-    const token = cookie.get("token")
- try {
-        const response = await axios.post(`${baseURL}/${path}`,
-        data,
-            {
-                headers:{
-                    Authorization:`Bearer ${token||token.token}`
-                }
-            }
-        )
-return response.data
-        
-    } catch (error) {
-        console.log(error.message)
-        return error.response.data
-        
-    }
-}
-
-
-
-
-
+  const token = cookie.get("token");
+  try {
+    const response = await axios.post(`${baseURL}/${path}`, data, {
+      headers: {
+        Authorization: `Bearer ${token || token.token}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.log(error.message);
+    return error.response.data;
+  }
+};
